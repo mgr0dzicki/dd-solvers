e625321c18489c2c8b5bd2d3d8501b78cd66c69a
Mieszko Grodzicki <mieszko.grodzicki@gmail.com>
2025-12-15 16:33:21 +0100

    new reference solvers experiments

---
+++ notebooks/run_reference_solvers_experiments.sh
@@ -0,0 +1,8 @@
+python test_reference_solvers.py 2 1 11
+python test_reference_solvers.py 2 1 12
+python test_reference_solvers.py 2 3 10
+python test_reference_solvers.py 2 5 9
+
+python test_reference_solvers.py 3 1 7
+python test_reference_solvers.py 3 2 6
+python test_reference_solvers.py 3 3 5
--- notebooks/test_reference_solvers.py
+++ notebooks/test_reference_solvers.py
@@ -4,8 +4,8 @@ from experiments import *
 import torch
 
 d = int(sys.argv[1])
-p = int(sys.argv[3]) if len(sys.argv) > 3 else 1
-fine_m = int(sys.argv[2])
+p = int(sys.argv[2])
+fine_m = int(sys.argv[3])
 
 cg_kwargs = {
     "maxiter": 1200,
@@ -15,25 +15,23 @@ cg_kwargs = {
 factory_kwargs = {
     "test_case": continuous_coefficient_2d if d == 2 else continuous_coefficient_3d,
     "polynomial_degree": p,
+    "setup_repetitions": 1,
+    "solution_warmup_steps": 0,
+    "solution_measurement_steps": 1,
+    "solution_repetitions": 10,
 }
 
-solvers = []
+solvers = [CUDSS()]
 for amg_config in AMGX.config_names:
     solvers.append(CG(AMGX(amg_config, torch.float32), **cg_kwargs))
-    # solvers.append(CG(AMGX(amg_config, torch.float64), **cg_kwargs))
-solvers.append(CUDSS())
 
-mesh_family = MeshFamily.simplices_then_cubes(
-    d=d,
-    fine=fine_m,
-    both=0,
-)
+mesh_family = UniformMeshes(d=d, m=fine_m)
 
 factory = ExperimentFactory(**factory_kwargs, mesh_family=mesh_family)
 for solver in solvers:
     factory.add(
         Experiment(
-            fine_m=fine_m + 1,
+            fine_m="S{fine_m}",
             solvers_m=None,
             coarse_m=None,
             solver=solver,
