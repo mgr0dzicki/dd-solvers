66656e1d4e6569279beac87d16bedbc164651d96
Mieszko Grodzicki <mieszko.grodzicki@gmail.com>
2026-01-09 15:04:37 +0000

    ;sahf;lsafdsa

--- notebooks/benchmark_batched_gemv.py
+++ notebooks/benchmark_batched_gemv.py
@@ -11,21 +11,30 @@ def mul_torch(A: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     return (A @ b[:, :, None].to(A.dtype)).to(b.dtype)
 
 
-def mul_custom(A: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
-    return dd_solvers.gemv_strided_batched(A, b)
+def mul_custom_irrespective(A: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
+    return dd_solvers.gemv_strided_batched(A, b, use_shared_memory=False)
 
+
+def mul_custom_synch(A: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
+    return dd_solvers.gemv_strided_batched(A, b, use_shared_memory=True)
+
+
+algorithms = {
+    "irrespective": mul_custom_irrespective,
+    "synchronized": mul_custom_synch,
+    # "torch": mul_torch,
+}
 
 MAX_ELEMENTS = 2**27
 
-ks = range(2, 130)
-# ks = range(90, 110)
+# ks = range(2, 130)
+ks = range(55, 69)
 results = []
 
 for k in tqdm.tqdm(ks):
     n = MAX_ELEMENTS // (k * k)
     for b_dtype in [torch.float32, torch.float64]:
-        # for A_dtype in [torch.float32, torch.float64, torch.float16, torch.bfloat16]:
-        for A_dtype in [torch.float16, torch.bfloat16]:
+        for A_dtype in [torch.float32, torch.float64, torch.float16, torch.bfloat16]:
             if A_dtype is torch.float64 and b_dtype is torch.float32:
                 continue  # skip unsupported combo
 
@@ -40,7 +49,7 @@ for k in tqdm.tqdm(ks):
             b = torch.randn((n, k), device="cuda", dtype=b_dtype)
             exact = A.to(torch.float64) @ b.to(torch.float64)[:, :, None]
 
-            for alg, alg_name in [(mul_custom, "custom")]:#, (mul_torch, "torch")]:
+            for alg_name, alg in algorithms.items():
                 err_norm = torch.linalg.norm(
                     exact.flatten() - alg(A, b).flatten()
                 ).item()
