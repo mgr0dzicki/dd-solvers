789f9a165a365e6d3813e03b0da3b66a0ca175d5
Mieszko Grodzicki <mieszko.grodzicki@gmail.com>
2026-01-10 18:55:52 +0100

    batched matrix vec section

--- src/dd_solvers/dd_solvers/gemv.py
+++ src/dd_solvers/dd_solvers/gemv.py
@@ -3,8 +3,16 @@ import torch
 
 @torch.library.custom_op("dd_solvers::gemv_strided_batched", mutates_args=())
 def gemv_strided_batched(
-    matrices: torch.Tensor, vectors: torch.Tensor, use_shared_memory: bool = False
+    matrices: torch.Tensor, vectors: torch.Tensor, use_shared_memory: bool | None = None
 ) -> torch.Tensor:
+    if use_shared_memory is None:
+        if vectors.dtype is torch.float32:
+            use_shared_memory = vectors.shape[1] > 15 and vectors.shape[1] <= 1024
+        elif vectors.dtype is torch.float64:
+            use_shared_memory = vectors.shape[1] > 5 and vectors.shape[1] <= 1024
+        else:
+            raise ValueError("Unsupported data type")
+
     return torch.ops.dd_solvers_gemv.gemv_strided_batched(
         matrices, vectors, use_shared_memory
     )
